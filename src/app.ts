bd46d1261f66e25a62e5772bee2e24784402b907
whitekiwi <jh145478@gmail.com>
2021-01-15 15:07:31 +0900

    error catch

--- src/app.ts
+++ src/app.ts
@@ -13,14 +13,19 @@ program.parse(process.argv)
 async function main() {
 	const feedName = (program.feedName || '').toUpperCase()
 
-	if (feedName === 'NAVER-FEED') {
-		const naverFeed = new NaverFeed()
-		await naverFeed.upload()
-	}
+	try {
+		if (feedName === 'NAVER-FEED') {
+			const naverFeed = new NaverFeed()
+			await naverFeed.upload()
+		}
 
-	if (feedName === 'GOOGLE-FEED') {
-		const googleFeed = new GoogleFeed()
-		await googleFeed.upload()
+		if (feedName === 'GOOGLE-FEED') {
+			const googleFeed = new GoogleFeed()
+			await googleFeed.upload()
+		}
+	} catch (e) {
+		console.log(e)
+		process.exit(1)
 	}
 
 	process.exit(0)
