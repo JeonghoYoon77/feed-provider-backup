46d4ba9181ccb902324f6dceb9de87faaa1ebca4
juunini <juuni.ni.i@gmail.com>
2021-02-25 16:30:26 +0900

    Fix form making for facebook feed rule

--- src/feeds/facebook-feed/form.ts
+++ src/feeds/facebook-feed/form.ts
@@ -1,14 +1,16 @@
 import { RowDataPacket } from 'mysql2/promise'
-import { parseInt } from 'lodash'
+import { parseInt, capitalize, isEmpty } from 'lodash'
 
 export class Form {
   private id: number;
   private title: string;
   private description: string;
-  private availability: string;
-  private condition: string;
-  private price: string;
-  private link: string;
+  private availability: string = 'available for order';
+  private condition: string = 'new';
+	private currency: string = 'KRW';
+  private price: number;
+	private linkPrefix: string = 'https://fetching.co.kr/product/detail.html?product_no=';
+  private productNo: number;
   private image_link: string;
   private brand: string;
 
@@ -16,20 +18,16 @@ export class Form {
 		id: number,
 		title: string,
 		description: string,
-		availability: string,
-		condition: string,
-		price: string,
-		link: string,
+		price: number,
+		productNo: number,
 		image_link: string,
 		brand: string,
 	) {
 		this.id = id
 		this.title = title
 		this.description = description
-		this.availability = availability
-		this.condition = condition
 		this.price = price
-		this.link = link
+		this.productNo = productNo
 		this.image_link = image_link
 		this.brand = brand
 	}
@@ -37,12 +35,12 @@ export class Form {
 	toObject() {
 		return {
 			id: this.id,
-			title: this.title,
-			description: this.description,
+			title: capitalize(this.title),
+			description: isEmpty(this.description) ? this.title : this.description,
 			availability: this.availability,
 			condition: this.condition,
-			price: this.price,
-			link: this.link,
+			price: `${this.price}${this.currency}`,
+			link: `${this.linkPrefix}${this.productNo}`,
 			image_link: this.image_link,
 			brand: this.brand,
 		}
@@ -54,10 +52,8 @@ function Format(rows: RowDataPacket[]): Form[] {
 		const id: number = parseInt(row.idx)
 		const title: string = row['item_name']
 		const description: string = row['item_description']
-		const availability: string = 'available for order'
-		const condition: string = 'new'
-		const price: string = makePrice(<number>row['final_price'])
-		const link: string = makeLink(<number>row['product_no'])
+		const price: number = <number>row['final_price']
+		const productNo: number = <number>row['product_no']
 		const imageLink: string = row['image_url']
 		const brand: string = row['brand_name']
 
@@ -65,25 +61,13 @@ function Format(rows: RowDataPacket[]): Form[] {
 			id,
 			title,
 			description,
-			availability,
-			condition,
 			price,
-			link,
+			productNo,
 			imageLink,
 			brand,
 		)
 	})
 	return contents
-}
-
-function makePrice(finalPrice: number): string {
-	const currency = 'KRW'
-	return finalPrice.toString() + currency
-}
-
-function makeLink(productNo: number): string {
-	const fetchingCafe24URL = 'https://fetching.co.kr/product/detail.html?product_no='
-	return fetchingCafe24URL + productNo.toString()
 }
 
 export default Format
