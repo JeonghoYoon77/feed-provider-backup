b38cc5e4f839ca3d98b0661c34ae67b9f5cee02d
최호승 <32726809+startergate@users.noreply.github.com>
2024-03-18 16:22:50 +0900

    refac: docker

--- docker/build/deploy.sh
+++ docker/build/deploy.sh
@@ -1,3 +1,5 @@
 aws ecr get-login-password --region ap-northeast-2 |  docker login --username AWS --password-stdin 605438638109.dkr.ecr.ap-northeast-2.amazonaws.com
 
-docker build -t 605438638109.dkr.ecr.ap-northeast-2.amazonaws.com/feed-provider:base --push .
+docker build -t 605438638109.dkr.ecr.ap-northeast-2.amazonaws.com/feed-provider:base -q .
+
+docker push 605438638109.dkr.ecr.ap-northeast-2.amazonaws.com/feed-provider:base
