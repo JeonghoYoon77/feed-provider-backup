dfbbf711382bd7ec4ba600b229cc0ca60bf4ed44
Hoseung Choi <me@startergate.dev>
2024-03-18 14:38:34 +0900

    feat: build on commit

--- docker/build/deploy.sh
+++ docker/build/deploy.sh
@@ -1,3 +1,3 @@
 aws ecr get-login-password --region ap-northeast-2 |  docker login --username AWS --password-stdin 605438638109.dkr.ecr.ap-northeast-2.amazonaws.com
 
-docker build -t 605438638109.dkr.ecr.ap-northeast-2.amazonaws.com/feed-provider:latest --push .
+docker build -t 605438638109.dkr.ecr.ap-northeast-2.amazonaws.com/feed-provider:base --push .
---
+++ docker/run.sh
@@ -0,0 +1,3 @@
+cd /home/feed-provider
+
+npm start $*
